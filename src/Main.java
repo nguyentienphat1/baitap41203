a934cf896aa6e4128449af38089c94bc03344e42
Khac Huy <nguyentienphat13@gmail.com>
2023-03-11 11:10:31 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int x=0;x<99;x++){
+            if (x%5==0){
+                System.out.println("la dung"+x);
+            }
+        }
+    }
+}
